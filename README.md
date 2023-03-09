9bb0229566def8b833d116ab40391005d1fe73e7
yemreeee <yunus@aeyazilim.com>
2023-03-09 17:56:31 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# namer_navigation_app
+
+A new Flutter project. Created from Colab.
+
+
+![Alt text](ss1.png)![Alt text](ss2.png)
---
+++ android/app/src/main/kotlin/com/example/namer_navigation_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.namer_navigation_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
